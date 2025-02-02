68d3a1b70f6ea1867015a71981ac6ec43615ba2a
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-02-02 13:44:12 +0100

    expose types

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   entry: {
     'math/index': 'src/math/index.ts',
     'geom/index': 'src/geom/index.ts',
+    'types/index': 'src/types/index.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
