f554170e5a2b673501fa539c4a8c0075ffd9a593
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-02-03 08:08:21 +0100

    create oscillator

--- packages/oscillator/src/index.ts
+++ packages/oscillator/src/index.ts
@@ -1,1 +1,5 @@
+import { waveFormFactory, type WaveformType, type WaveformParams } from '@threeaio/utils/animation';
+// or
+import { sine, triangle, sawtooth } from '@threeaio/utils/animation';
+
 export {};
