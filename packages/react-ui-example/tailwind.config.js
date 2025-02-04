de62789bc1dba2ec9708c81c914ff0b2af1090ed
Nikolaj Sokolowski <n.sokolowski@neusta.de>
2025-02-04 16:58:24 +0100

    handle env - WIP

--- packages/react-ui-example/tailwind.config.js
+++ packages/react-ui-example/tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
+const isMonorepo = process.env.MONOREPO === 'true';
+
+console.log('isMonorepo here', isMonorepo);
+
+const paths = isMonorepo
+  ? {
+      content: "../react-ui/dist/**/*.{js,ts,jsx,tsx}",
+      preset: "@threeaio/react-ui/tailwind-preset"
+    }
+  : {
+      content: "node_modules/@threeaio/react-ui/dist/**/*.{js,ts,jsx,tsx}",
+      preset: "@threeaio/react-ui/tailwind-preset"
+    };
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "../react-ui/src/**/*.{js,ts,jsx,tsx}",
+    paths.content
   ],
   presets: [
-    require("@threeaio/react-ui/tailwind-preset")
+    require(paths.preset)
   ]
 } 
